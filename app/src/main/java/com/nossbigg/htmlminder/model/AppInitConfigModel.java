c15cf63cee5b15580aa52fd41a88832d82b4a957
Cheng Gibson <chenggibson@gmail.com>
2016-09-10 02:18:29 +0800

    Add config model files for json persistence storage

---
+++ app/src/main/java/com/nossbigg/htmlminder/model/AppInitConfigModel.java
@@ -0,0 +1,11 @@
+package com.nossbigg.htmlminder.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Gibson on 9/9/2016.
+ */
+public class AppInitConfigModel implements Serializable {
+  public String APP_DIR = "";
+  public String HERE_DIR = "";
+}
