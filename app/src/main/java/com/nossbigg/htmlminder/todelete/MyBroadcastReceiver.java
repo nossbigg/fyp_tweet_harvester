edd95dc027f7437b4d6e73d3aca6284ea1dcda5c
Cheng Gibson <chenggibson@gmail.com>
2016-09-11 01:51:53 +0800

    Comment out todelete classes (to prevent dependency build errors)

--- app/src/main/java/com/nossbigg/htmlminder/todelete/MyBroadcastReceiver.java
+++ app/src/main/java/com/nossbigg/htmlminder/todelete/MyBroadcastReceiver.java
@@ -1,19 +1,19 @@
-package com.nossbigg.htmlminder.todelete;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
-
-/**
- * Created by Gibson on 8/12/2016.
- */
-public class MyBroadcastReceiver extends BroadcastReceiver {
-  public void onReceive(Context context, Intent intent) {
-    Log.d("UNIQUE_TAG", "inside MyBroadcastReceiver");
-    String response = intent.getStringExtra("response");
-    Toast.makeText(context, "received the Intent's message: " + response, Toast.LENGTH_LONG).show();
-    Log.d("UNIQUE_TAG", response);
-  }
-}
+//package com.nossbigg.htmlminder.todelete;
+//
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.util.Log;
+//import android.widget.Toast;
+//
+///**
+// * Created by Gibson on 8/12/2016.
+// */
+//public class MyBroadcastReceiver extends BroadcastReceiver {
+//  public void onReceive(Context context, Intent intent) {
+//    Log.d("UNIQUE_TAG", "inside MyBroadcastReceiver");
+//    String response = intent.getStringExtra("response");
+//    Toast.makeText(context, "received the Intent's message: " + response, Toast.LENGTH_LONG).show();
+//    Log.d("UNIQUE_TAG", response);
+//  }
+//}
