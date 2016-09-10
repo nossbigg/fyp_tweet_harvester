3649f2dd4979d21dcebdc289860825fa37c032c8
Cheng Gibson <chenggibson@gmail.com>
2016-09-11 00:13:33 +0800

    Formatting

--- app/src/main/java/com/nossbigg/htmlminder/exception/HTMLWorkerCallFailedException.java
+++ app/src/main/java/com/nossbigg/htmlminder/exception/HTMLWorkerCallFailedException.java
@@ -3,7 +3,7 @@ package com.nossbigg.htmlminder.exception;
 /**
  * Created by Gibson on 9/6/2016.
  */
-public class HTMLWorkerCallFailedException extends  HTMLWorkerException {
+public class HTMLWorkerCallFailedException extends HTMLWorkerException {
   public HTMLWorkerCallFailedException(String message) {
     super(message);
   }
