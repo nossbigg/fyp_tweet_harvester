c6d60f6fb7d691a28bbda429270c0904c7656f81
Cheng Gibson <chenggibson@gmail.com>
2016-09-11 00:19:44 +0800

    Add - tryDeleteFile (to save try/catch code clutter, when catching is not needed) - gzip compression method (untested)

--- app/src/main/java/com/nossbigg/htmlminder/utils/FileUtilsCustom.java
+++ app/src/main/java/com/nossbigg/htmlminder/utils/FileUtilsCustom.java
@@ -23,10 +23,56 @@ public class FileUtilsCustom {
   public static void saveToFile(String path, String content, boolean isAppend) throws IOException {
     File file = new File(path);
 
-    if(file.exists()){
+    if (file.exists()) {
       FileUtils.write(file, content, isAppend);
     } else {
       FileUtils.write(file, content);
     }
+  }
+
+  public static boolean tryDeleteFile(String path){
+    File f = new File(path);
+    try{
+      FileUtils.forceDelete(f);
+      return true;
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  public static boolean makeGzipFromFile(String sourcePath, String zipFilePath) {
+    // check if file exists
+    File sourceFile = new File(sourcePath);
+    if (!sourceFile.exists()) return false;
+
+    // write to file (using direct input from file, no need save response)
+    // ref: https://www.mkyong.com/java/how-to-compress-a-file-in-gzip-format/
+    byte[] buffer = new byte[1024];
+    GZIPOutputStream gzos = null;
+    FileInputStream in = null;
+    try {
+      // open streams
+      gzos = new GZIPOutputStream(new FileOutputStream(zipFilePath));
+      in = new FileInputStream(sourcePath);
+
+      // read/write files
+      int len;
+      while ((len = in.read(buffer)) > 0) {
+        gzos.write(buffer, 0, len);
+      }
+    } catch (IOException ex) {
+      // problems with process, make file failed
+      return false;
+    } finally {
+      try {
+        // close all streams
+        in.close();
+        gzos.finish();
+        gzos.close();
+      } catch (IOException e) {
+      }
+    }
+
+    return true;
   }
 }
