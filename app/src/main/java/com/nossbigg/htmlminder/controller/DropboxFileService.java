2fea50bf4c1b4b9965c81dbbe91834f03ec90936
Cheng Gibson <chenggibson@gmail.com>
2016-09-11 00:51:38 +0800

    Add DropboxFileService class template

---
+++ app/src/main/java/com/nossbigg/htmlminder/controller/DropboxFileService.java
@@ -0,0 +1,13 @@
+package com.nossbigg.htmlminder.controller;
+
+/**
+ * API for Dropbox-related actions
+ *
+ * Created by Gibson on 9/8/2016.
+ */
+public class DropboxFileService {
+  // TODO DropboxFileService
+
+  // for gradle
+  //  compile 'com.dropbox.core:dropbox-core-sdk:2.1.1'
+}
